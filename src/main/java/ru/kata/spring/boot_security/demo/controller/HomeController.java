de7f2e01696deb66906d9d1b11e33608f781133d
Admin <pyanton112@gmail.com>
2023-08-27 14:03:20 +0500

    view users in table

--- src/main/java/ru/kata/spring/boot_security/demo/controller/HomeController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/HomeController.java
@@ -1,32 +1,48 @@
 package ru.kata.spring.boot_security.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.security.Principal;
 
 @Controller
-public class HomeContoller {
+public class HomeController {
+    private final UserService userService;
+
+    @Autowired
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
     @GetMapping("/")
     public String getHome() {
         return "redirect:/user";
     }
     @GetMapping("/user")
-    public String getUser() {
+    public String getUser(Principal principal, Model model) {
+        User user = userService.findUserByEmail(principal.getName());
+        model.addAttribute(user);
         return "user";
     }
     @GetMapping("/admin")
-    public String getAdmin() {
+    public String getAdmin(@RequestParam(name = "count", defaultValue = "15") int count, Model model) {
+        model.addAttribute("users", userService.listUsersCount(count));
         return "admin";
     }
     @GetMapping("/login")
     public String getLogin() {
         return "login";
     }
+
 
     @PostMapping("/logout") // POST-запрос для фактического выхода
     public String customLogout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
--- src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -7,20 +7,25 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.dao.UserDao;
 import ru.kata.spring.boot_security.demo.model.Role;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repository.UserRepository;
 
 import javax.transaction.Transactional;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final UserDao userDao;
+
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserDao userDao, UserRepository userRepository) {
+        this.userDao = userDao;
         this.userRepository = userRepository;
     }
 
@@ -44,6 +49,20 @@ public class UserService implements UserDetailsService {
 
     private Collection<? extends GrantedAuthority> mapRoles(Collection<Role> roles) {
         return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public List<User> listUsersCount(int count) {
+        List<User> listUsers = listUsers();
+        if (count >= 15) {
+            return listUsers;
+        } else {
+            return listUsers.subList(0, Math.min(count, listUsers.size()));
+        }
+    }
+    @Transactional
+    public List<User> listUsers() {
+        return userDao.listUsers();
     }
 
 }
